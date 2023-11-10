0dbbaf6c4dde8c95bd49af8230486ecd8bce3671
sangdol <priceelees@gmail.com>
2023-11-10 15:31:33 +0900

    feat(Application): 게임을 시작하는 기능 정의

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,19 @@
 package christmas;
 
+import christmas.controller.PlannerMainController;
+import christmas.view.inputview.PlannerInputView;
+import christmas.view.outputview.PlannerOutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        PlannerInputView plannerInputView = new PlannerInputView();
+        PlannerOutputView plannerOutputView = new PlannerOutputView();
+        PlannerMainController plannerMainController = new PlannerMainController(
+                plannerInputView,
+                plannerOutputView
+        );
+
+        plannerMainController.start();
     }
 }
