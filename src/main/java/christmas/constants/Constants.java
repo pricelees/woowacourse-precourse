b985a6118fdd734c4924f64cc8bd6ea28b5f61b6
sangdol <priceelees@gmail.com>
2023-11-10 13:41:04 +0900

    feat(Constants): 프로그램 상에서 공통적으로 쓰이는 상수를 정의

---
+++ src/main/java/christmas/constants/Constants.java
@@ -0,0 +1,8 @@
+package christmas.constants;
+
+public class Constants {
+    public static final String LINE_SEPARATOR = System.lineSeparator();
+    public static final String NONE = "없음";
+    public static final int ZERO = 0;
+    public static final int ONE = 1;
+}
