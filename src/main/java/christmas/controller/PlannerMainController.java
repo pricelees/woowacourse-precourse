823e4f52f3b56ae4d219a21579058886576369ee
sangdol <priceelees@gmail.com>
2023-11-10 15:47:45 +0900

    feat(PlannerMainController): View와 Domain을 이용하여 플래너를 실행하는 기능 정의

---
+++ src/main/java/christmas/controller/PlannerMainController.java
@@ -0,0 +1,33 @@
+package christmas.controller;
+
+import christmas.constants.menu.Menu;
+import christmas.domain.SelectedMenu;
+import christmas.domain.WootecoPlanner;
+import christmas.view.inputview.PlannerInputView;
+import christmas.view.outputview.PlannerOutputView;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+public record PlannerMainController(PlannerInputView plannerInputView, PlannerOutputView plannerOutputView) {
+    public void start() {
+        plannerOutputView.printWelcomeMessage();
+        LocalDateTime visitDate = plannerInputView.receiveDateToVisit();
+        Map<Menu, Integer> menuToOrder = plannerInputView.receiveMenuToOrder();
+        plannerOutputView.printPreviewMessage(visitDate.getDayOfMonth());
+
+        SelectedMenu selectedMenu = new SelectedMenu(menuToOrder);
+        WootecoPlanner wootecoPlanner = WootecoPlanner.valueOf(visitDate, selectedMenu);
+
+        printAllInfo(wootecoPlanner);
+    }
+
+    private void printAllInfo(WootecoPlanner wootecoPlanner) {
+        plannerOutputView.printOrderedMenu(wootecoPlanner.showOrderedMenu());
+        plannerOutputView.printAmountBeforeDiscount(wootecoPlanner.showAmountBeforeDiscount());
+        plannerOutputView.printFreeMenu(wootecoPlanner.showFreeMenu());
+        plannerOutputView.printDiscountDescription(wootecoPlanner.showDiscountDescription());
+        plannerOutputView.printTotalDiscountAmount(wootecoPlanner.showTotalDiscountedAmount());
+        plannerOutputView.printExpectedAmount(wootecoPlanner.showExpectedAmount());
+        plannerOutputView.printEventBadge(wootecoPlanner.showEventBadge());
+    }
+}
