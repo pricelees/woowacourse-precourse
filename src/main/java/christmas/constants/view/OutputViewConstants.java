2c521fdbeea8780bea5156e700c20618a879b4ba
sangdol <priceelees@gmail.com>
2023-11-10 15:32:44 +0900

    feat(OutputViewConstants): 콘솔에 출력할 때 사용되는 상수 정의

---
+++ src/main/java/christmas/constants/view/OutputViewConstants.java
@@ -0,0 +1,15 @@
+package christmas.constants.view;
+
+public class OutputViewConstants {
+    public static final String WELCOME_MESSAGE =
+            "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+    public static final String PREVIEW_BENEFITS_MESSAGE =
+            "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
+    public static final String ORDERED_MENU = "<주문 메뉴>";
+    public static final String TOTAL_AMOUNT_BEFORE_DISCOUNT = "<할인 전 총주문 금액>";
+    public static final String MENU_FOR_FREE = "<증정 메뉴>";
+    public static final String BENEFITS_DESCRIPTION = "<혜택 내역>";
+    public static final String TOTAL_BENEFITS_AMOUNT = "<총혜택 금액>";
+    public static final String PRICE_AFTER_DISCOUNT = "<할인 후 예상 결제 금액>";
+    public static final String EVENT_BADGE = "<12월 이벤트 배지>";
+}
