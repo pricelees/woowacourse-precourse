24c06cfac1126d1d753fd0e2a29899a561629a61
sangdol <priceelees@gmail.com>
2023-11-11 13:45:14 +0900

    refactor(Constants): String.format 메서드를 문자열 덧셈으로 바꾸기 위한 상수 정의

--- src/main/java/christmas/constants/Constants.java
+++ src/main/java/christmas/constants/Constants.java
@@ -3,6 +3,8 @@ package christmas.constants;
 public class Constants {
     public static final String LINE_SEPARATOR = System.lineSeparator();
     public static final String NONE = "없음";
+    public static final String COLON_WITH_SPACE = ": ";
     public static final int ZERO = 0;
     public static final int ONE = 1;
+
 }
