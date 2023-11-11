48d3b02798340a58b297700aa50d3221a707d0d3
sangdol <priceelees@gmail.com>
2023-11-11 19:41:26 +0900

    refactor(Constants): 최대 메뉴 개수를 지정하는 상수 추가

--- src/main/java/christmas/constants/Constants.java
+++ src/main/java/christmas/constants/Constants.java
@@ -4,6 +4,7 @@ public class Constants {
     public static final String LINE_SEPARATOR = System.lineSeparator();
     public static final String NONE = "없음";
     public static final String COLON_WITH_SPACE = ": ";
+    public static final int MAXIMUM_TOTAL_MENU_COUNT = 20;
     public static final int ZERO = 0;
     public static final int ONE = 1;
 
