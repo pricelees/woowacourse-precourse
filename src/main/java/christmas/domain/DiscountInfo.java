07989b6258cd83430ee1dbae2da26d496b36ad83
sangdol <priceelees@gmail.com>
2023-11-11 13:47:31 +0900

    refactor(DiscountInfo): 새로 정의된 Customer 객체를 이용하도록 수정

--- src/main/java/christmas/domain/DiscountInfo.java
+++ src/main/java/christmas/domain/DiscountInfo.java
@@ -4,51 +4,44 @@ import christmas.constants.Constants;
 import christmas.discount.ChampagneDiscount;
 import christmas.discount.DiscountStrategy;
 import christmas.util.DiscountProviderByDate;
-import java.time.LocalDateTime;
 import java.util.List;
 
 public record DiscountInfo(
-        LocalDateTime visitTime,
-        SelectedMenu selectedMenu,
+        Customer customer,
         List<DiscountStrategy> discountTypes) {
-    private static final int MINIMUM_AMOUNT_TO_GET_CHAMPAGNE = 120_000;
     private static final int MINIMUM_AMOUNT_TO_DISCOUNT = 10_000;
 
-    public static DiscountInfo valueOf(LocalDateTime visitTime, SelectedMenu selectedMenu) {
-        DiscountProviderByDate provider = new DiscountProviderByDate(visitTime);
+    public static DiscountInfo valueOf(Customer customer) {
+        DiscountProviderByDate provider = new DiscountProviderByDate(customer.dateToVisit());
         List<DiscountStrategy> discountTypes = provider.provide();
 
-        if (selectedMenu.getTotalAmountBeforeDiscount() >= MINIMUM_AMOUNT_TO_GET_CHAMPAGNE) {
+        if (customer.canReceiveFreeChampagne()) {
             discountTypes.add(new ChampagneDiscount());
         }
 
-        return new DiscountInfo(visitTime, selectedMenu, discountTypes);
+        return new DiscountInfo(customer, discountTypes);
     }
 
     public int calculateBenefitsAmount() {
-        if (selectedMenu.getTotalAmountBeforeDiscount() < MINIMUM_AMOUNT_TO_DISCOUNT) {
+        if (customer.cannotParticipateEvent()) {
             return Constants.ZERO;
         }
         return discountTypes.stream()
-                .mapToInt(discountType -> discountType.getDiscountAmount(visitTime, selectedMenu))
+                .mapToInt(discountType -> discountType.getDiscountAmount(customer))
                 .sum();
     }
 
     public int calculateDiscountAmount() {
-        if (selectedMenu.getTotalAmountBeforeDiscount() < MINIMUM_AMOUNT_TO_DISCOUNT) {
+        if (customer.cannotParticipateEvent()) {
             return Constants.ZERO;
         }
         return discountTypes.stream()
                 .filter(discountType -> discountType.getClass() != ChampagneDiscount.class)
-                .mapToInt(discountType -> discountType.getDiscountAmount(visitTime, selectedMenu))
+                .mapToInt(discountType -> discountType.getDiscountAmount(customer))
                 .sum();
     }
 
     public int calculateAmountAfterDiscount() {
-        return selectedMenu().getTotalAmountBeforeDiscount() + calculateDiscountAmount();
-    }
-
-    public boolean hasFreeChampagne() {
-        return selectedMenu.getTotalAmountBeforeDiscount() >= MINIMUM_AMOUNT_TO_GET_CHAMPAGNE;
+        return customer.orderAmount() + calculateDiscountAmount();
     }
 }
