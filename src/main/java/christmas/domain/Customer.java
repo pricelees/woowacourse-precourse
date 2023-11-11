c08b39843d695656e314f95e593c26c21e1c20db
sangdol <priceelees@gmail.com>
2023-11-11 20:26:20 +0900

    refactor(Customer): 고객의 날짜 정보에 대한 예외 검증 추가

--- src/main/java/christmas/domain/Customer.java
+++ src/main/java/christmas/domain/Customer.java
@@ -1,10 +1,17 @@
 package christmas.domain;
 
+import christmas.constants.domain.DomainErrorMessage;
+import christmas.constants.time.EventTime;
 import java.time.LocalDateTime;
+import java.time.Month;
 
 public record Customer(LocalDateTime dateToVisit, SelectedMenu selectedMenu) {
     private static final int MINIMUM_AMOUNT_TO_GET_CHAMPAGNE = 120_000;
     private static final int MINIMUM_AMOUNT_TO_PARTICIPATE_EVENT = 10_000;
+
+    public Customer {
+        validateDate(dateToVisit);
+    }
 
     public int dayOfMonthToVisit() {
         return dateToVisit.getDayOfMonth();
@@ -32,5 +39,23 @@ public record Customer(LocalDateTime dateToVisit, SelectedMenu selectedMenu) {
 
     public boolean cannotParticipateEvent() {
         return orderAmount() < MINIMUM_AMOUNT_TO_PARTICIPATE_EVENT;
+    }
+
+    // LocalDateTime을 이용하기에, "일" 정보에 대해 예외를 검증할 필요는 없음.
+    private void validateDate(LocalDateTime dateToVisit) {
+        validateYear(dateToVisit.getYear());
+        validateMonth(dateToVisit.getMonth());
+    }
+
+    private void validateYear(int year) {
+        if (year != EventTime.YEAR) {
+            throw new IllegalArgumentException(DomainErrorMessage.INVALID_YEAR_ERROR.getErrorMessage());
+        }
+    }
+
+    private void validateMonth(Month month) {
+        if (month != EventTime.MONTH) {
+            throw new IllegalArgumentException(DomainErrorMessage.INVALID_MONTH_ERROR.getErrorMessage());
+        }
     }
 }
