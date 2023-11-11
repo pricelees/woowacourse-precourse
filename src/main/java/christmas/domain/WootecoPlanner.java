6516632beade585fff29e29bfa691400fefe8d1a
sangdol <priceelees@gmail.com>
2023-11-11 13:13:18 +0900

    refactor(WootecoPlanner): 이전의 수정사항을 반영한 코드 수정
    < description >
    1. 할인 금액을 음수로 반환하도록 하여, format을 정의하던 상수가 필요 없어짐
    2. 할인 금액과 혜택 금액을 DiscountInfo에서 구분한 내용 반영

--- src/main/java/christmas/domain/WootecoPlanner.java
+++ src/main/java/christmas/domain/WootecoPlanner.java
@@ -6,15 +6,14 @@ import java.time.LocalDateTime;
 import java.util.stream.Collectors;
 
 public record WootecoPlanner(
-        LocalDateTime date,
+        LocalDateTime dateToVisit,
         SelectedMenu selectedMenu,
         DiscountInfo discountInfo,
         BadgeInfo badgeInfo
 ) {
-    private static final String DISCOUNT_DESCRIPTION_FORMAT = "%s: -%s";
-    private static final String DISCOUNT_AMOUNT_FORMAT = "-%s";
-    private static final String ONE_CHAMPAGNE = "샴페인 1개";
     private static final DecimalFormat PRICE_FORMATTER = new DecimalFormat("###,###원");
+    private static final String COLON_WITH_SPACE = ": ";
+    private static final String ONE_CHAMPAGNE = "샴페인 1개";
     private static final int MINIMUM_AMOUNT_FOR_EVENT = 10_000;
 
     public static WootecoPlanner valueOf(LocalDateTime date, SelectedMenu selectedMenu) {
@@ -26,51 +25,45 @@ public record WootecoPlanner(
     }
 
     public String showAmountBeforeDiscount() {
-        return PRICE_FORMATTER.format(discountInfo.calculateAmountBeforeDiscount());
+        return PRICE_FORMATTER.format(selectedMenu.getTotalAmountBeforeDiscount());
     }
 
     public String showFreeMenu() {
         if (discountInfo.hasFreeChampagne()) {
             return ONE_CHAMPAGNE;
         }
+
         return Constants.NONE;
     }
 
     public String showDiscountDescription() {
-        if (selectedMenu.getTotalAmountBeforeDiscount() < MINIMUM_AMOUNT_FOR_EVENT) {
+        if (cantParticipateEvent()) {
             return Constants.NONE;
         }
 
         return discountInfo.discountTypes().stream()
-                .filter(type -> type.getDiscountAmount(date, selectedMenu) > Constants.ZERO)
-                .map(type -> String.format(
-                                DISCOUNT_DESCRIPTION_FORMAT,
-                                type.getTypeName(),
-                                PRICE_FORMATTER.format(type.getDiscountAmount(date, selectedMenu))
-                        )
-                )
+                .map(type -> type.getTypeName()
+                        + COLON_WITH_SPACE
+                        + PRICE_FORMATTER.format(type.getDiscountAmount(dateToVisit, selectedMenu)))
                 .collect(Collectors.joining(System.lineSeparator()));
     }
 
-    public String showTotalDiscountedAmount() {
-        int calculatedDiscountAmount = discountInfo.calculateDiscountAmount();
-        String formattedDiscountAmount = PRICE_FORMATTER.format(calculatedDiscountAmount);
-
-        if (calculatedDiscountAmount == Constants.ZERO) {
-            return formattedDiscountAmount;
-        }
-
-        return String.format(DISCOUNT_AMOUNT_FORMAT, formattedDiscountAmount);
+    public String showTotalBenefitsAmount() {
+        return PRICE_FORMATTER.format(discountInfo.calculateBenefitsAmount());
     }
 
-    public String showExpectedAmount() {
+    public String showExpectedAmountToPay() {
         return PRICE_FORMATTER.format(discountInfo.calculateAmountAfterDiscount());
     }
 
     public String showEventBadge() {
-        if (selectedMenu.getTotalAmountBeforeDiscount() < MINIMUM_AMOUNT_FOR_EVENT) {
+        if (cantParticipateEvent()) {
             return Constants.NONE;
         }
         return badgeInfo.getBadgeName(discountInfo.calculateAmountAfterDiscount());
+    }
+
+    private boolean cantParticipateEvent() {
+        return selectedMenu.getTotalAmountBeforeDiscount() < MINIMUM_AMOUNT_FOR_EVENT;
     }
 }
