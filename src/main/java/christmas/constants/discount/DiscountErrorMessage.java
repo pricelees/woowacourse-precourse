3d62830beb84b1880f1a9ede000960ae50a181ab
sangdol <priceelees@gmail.com>
2023-11-11 10:44:23 +0900

    feat(DiscountErrorMessage): 할인 방식에 사용되는 예외 메시지 정의

---
+++ src/main/java/christmas/constants/discount/DiscountErrorMessage.java
@@ -0,0 +1,9 @@
+package christmas.constants.discount;
+
+public class DiscountErrorMessage {
+    public static final String YEAR_AND_MONTH_ERROR = "[ERROR] 이벤트는 2023년 12월에만 진행됩니다.";
+    public static final String NOT_BEFORE_CHRISTMAS_ERROR = "[ERROR] 크리스마스 디데이 할인은 1일부터 25일까지만 진행합니다.";
+    public static final String NOT_SPECIAL_DAY_ERROR = "[ERROR] 특별 할인이 적용되는 날이 아닙니다.";
+    public static final String NOT_WEEKDAY_ERROR = "[ERROR] 해당 이벤트는 평일에만 진행됩니다.";
+    public static final String NOT_WEEKEND_ERROR = "[ERROR] 해당 이벤트는 주말에만 진행됩니다.";
+}
