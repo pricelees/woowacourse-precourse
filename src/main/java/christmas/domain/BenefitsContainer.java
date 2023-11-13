1c767fc8f50ac22a90317a5a9a8e41c7db638d04
sangdol <priceelees@gmail.com>
2023-11-13 11:43:25 +0900

    rename(DiscountInfo): 클래스명을 BenefitsContainer로 수정

--- src/main/java/christmas/domain/BenefitsContainer.java
+++ src/main/java/christmas/domain/BenefitsContainer.java
@@ -1,6 +1,5 @@
 package christmas.domain;
 
-import christmas.constants.Constants;
 import christmas.discount.ChampagneDiscount;
 import christmas.discount.DiscountStrategy;
 import christmas.util.DiscountProviderByDate;
@@ -8,35 +7,29 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public record DiscountInfo(
+public record BenefitsContainer(
         Customer customer,
         List<DiscountStrategy> discountTypes) {
-    public static DiscountInfo valueOf(Customer customer) {
+    public static BenefitsContainer of(Customer customer) {
         DiscountProviderByDate provider = new DiscountProviderByDate(customer.dateToVisit());
         List<DiscountStrategy> discountTypes = provider.provide();
 
         if (customer.canReceiveFreeChampagne()) {
             List<DiscountStrategy> discountsWithChampagne = new ArrayList<>(discountTypes);
             discountsWithChampagne.add(new ChampagneDiscount());
-            return new DiscountInfo(customer, Collections.unmodifiableList(discountsWithChampagne));
+            return new BenefitsContainer(customer, Collections.unmodifiableList(discountsWithChampagne));
         }
 
-        return new DiscountInfo(customer, discountTypes);
+        return new BenefitsContainer(customer, discountTypes);
     }
 
     public int calculateBenefitsAmount() {
-        if (customer.cannotParticipateEvent()) {
-            return Constants.ZERO;
-        }
         return discountTypes.stream()
                 .mapToInt(discountType -> discountType.getDiscountAmount(customer))
                 .sum();
     }
 
     public int calculateDiscountAmount() {
-        if (customer.cannotParticipateEvent()) {
-            return Constants.ZERO;
-        }
         return discountTypes.stream()
                 .filter(discountType -> discountType.getClass() != ChampagneDiscount.class)
                 .mapToInt(discountType -> discountType.getDiscountAmount(customer))
