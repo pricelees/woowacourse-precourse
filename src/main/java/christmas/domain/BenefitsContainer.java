9eecbd7f4a03e17b35f5627493237ed24bced0ff
sangdol <priceelees@gmail.com>
2023-11-14 12:35:33 +0900

    refactor(BenefitsContainer): 할인 종류를 나타내는 필드를 제거하고, 내부의 private을 통해 얻어내도록 수정

--- src/main/java/christmas/domain/BenefitsContainer.java
+++ src/main/java/christmas/domain/BenefitsContainer.java
@@ -1,36 +1,25 @@
 package christmas.domain;
 
+import christmas.constants.Constants;
 import christmas.discount.ChampagneDiscount;
 import christmas.discount.DiscountStrategy;
 import christmas.util.DiscountProviderByDate;
+import christmas.util.PriceFormatter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-public record BenefitsContainer(
-        Customer customer,
-        List<DiscountStrategy> discountTypes) {
-    public static BenefitsContainer of(Customer customer) {
-        DiscountProviderByDate provider = new DiscountProviderByDate(customer.dateToVisit());
-        List<DiscountStrategy> discountTypes = provider.provide();
-
-        if (customer.canReceiveFreeChampagne()) {
-            List<DiscountStrategy> discountsWithChampagne = new ArrayList<>(discountTypes);
-            discountsWithChampagne.add(new ChampagneDiscount());
-            return new BenefitsContainer(customer, Collections.unmodifiableList(discountsWithChampagne));
-        }
-
-        return new BenefitsContainer(customer, discountTypes);
-    }
-
+public record BenefitsContainer(Customer customer) {
     public int calculateBenefitsAmount() {
-        return discountTypes.stream()
+        return discountTypeStream()
                 .mapToInt(discountType -> discountType.getDiscountAmount(customer))
                 .sum();
     }
 
     public int calculateDiscountAmount() {
-        return discountTypes.stream()
+        return discountTypeStream()
                 .filter(discountType -> discountType.getClass() != ChampagneDiscount.class)
                 .mapToInt(discountType -> discountType.getDiscountAmount(customer))
                 .sum();
@@ -38,5 +27,33 @@ public record BenefitsContainer(
 
     public int calculateAmountAfterDiscount() {
         return customer.orderAmount() + calculateDiscountAmount();
+    }
+
+    public String getDiscountDescription() {
+        return discountTypeStream()
+                .map(type -> type.getTypeName()
+                        + Constants.COLON_WITH_SPACE
+                        + PriceFormatter.format(type.getDiscountAmount(customer)))
+                .collect(Collectors.joining(Constants.LINE_SEPARATOR));
+    }
+
+    private Stream<DiscountStrategy> discountTypeStream() {
+        return receiveDiscountTypes().stream();
+    }
+
+    private List<DiscountStrategy> receiveDiscountTypes() {
+        DiscountProviderByDate provider = new DiscountProviderByDate(customer.dateToVisit());
+        List<DiscountStrategy> result = provider.provide();
+
+        return addChampagneDiscountIfPossible(result);
+    }
+
+    private List<DiscountStrategy> addChampagneDiscountIfPossible(final List<DiscountStrategy> discountTypes) {
+        List<DiscountStrategy> result = new ArrayList<>(discountTypes);
+        if (customer.canReceiveFreeChampagne()) {
+            result.add(new ChampagneDiscount());
+        }
+
+        return Collections.unmodifiableList(result);
     }
 }
