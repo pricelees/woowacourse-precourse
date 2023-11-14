7b0ed8770687f9c9d956c7ffeeb21437b55b2b4d
sangdol <priceelees@gmail.com>
2023-11-15 01:05:49 +0900

    refactor(CustomerServiceWithDiscount): 새로 정의한 BadgeProvider, DiscountProvider를 이용하도록 수정

--- src/main/java/christmas/service/CustomerServiceWithDiscount.java
+++ src/main/java/christmas/service/CustomerServiceWithDiscount.java
@@ -1,18 +1,16 @@
 package christmas.service;
 
 import christmas.constants.Constants;
-import christmas.domain.BadgeContainer;
-import christmas.domain.BenefitsContainer;
+import christmas.domain.BadgeProvider;
 import christmas.domain.Customer;
+import christmas.domain.DiscountProvider;
 import christmas.util.PriceFormatter;
 
 public class CustomerServiceWithDiscount extends CustomerService {
     private static final String ONE_CHAMPAGNE = "샴페인 1개";
-    private final BenefitsContainer benefitsContainer;
 
     public CustomerServiceWithDiscount(Customer customer) {
         super(customer);
-        this.benefitsContainer = new BenefitsContainer(customer);
     }
 
     @Override
@@ -25,22 +23,25 @@ public class CustomerServiceWithDiscount extends CustomerService {
 
     @Override
     public String showDiscountDescription() {
-        return benefitsContainer.getDiscountDescription();
+        return DiscountProvider.allDiscountsDescription(customer);
     }
 
     @Override
     public String showTotalBenefitsAmount() {
-        return PriceFormatter.format(benefitsContainer.calculateBenefitsAmount());
+        return PriceFormatter.format(DiscountProvider.totalBenefitsAmount(customer));
     }
 
     @Override
     public String showExpectedAmountToPay() {
-        return PriceFormatter.format(benefitsContainer.calculateAmountAfterDiscount());
+        return PriceFormatter.format(calculateAmountAfterDiscount());
     }
 
     @Override
     public String showEventBadge() {
-        BadgeContainer badgeContainer = new BadgeContainer(benefitsContainer.calculateAmountAfterDiscount());
-        return badgeContainer.getBadgeName();
+        return BadgeProvider.getBadgeName(calculateAmountAfterDiscount());
+    }
+
+    private int calculateAmountAfterDiscount() {
+        return customer.orderAmount() + DiscountProvider.actualDiscountAmount(customer);
     }
 }
