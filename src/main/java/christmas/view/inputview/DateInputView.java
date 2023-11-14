a37b0b2fb829606d12d78dbe69fd46e146d21d09
sangdol <priceelees@gmail.com>
2023-11-14 14:47:25 +0900

    comment(DateInputView): 날짜 범위 예외 검증에 대한 주석 추가

--- src/main/java/christmas/view/inputview/DateInputView.java
+++ src/main/java/christmas/view/inputview/DateInputView.java
@@ -12,6 +12,8 @@ public class DateInputView {
         String input = Console.readLine();
         validateDateFormat(input);
 
+        // 날짜가 1일부터 31일까지인지에 대한 검증
+        // 1일부터 31일 까지의 값이 아니면, LocalDateTime 객체를 만들 때 DateTimeException이 발생함
         try {
             return LocalDateTime.of(
                     EventTime.YEAR, EventTime.MONTH, Integer.parseInt(input),
