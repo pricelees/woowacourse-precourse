e84caaf09778bdeaea8617f880dbbca79078d269
sangdol <priceelees@gmail.com>
2023-11-15 08:16:56 +0900

    test(TestConstants): 테스트 코드에서 공통적으로 쓰이는 상수를 정의

---
+++ src/test/java/christmas/TestConstants.java
@@ -0,0 +1,9 @@
+package christmas;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class TestConstants {
+    public static final int RANDOM_DAY_OF_MONTH = Randoms.pickNumberInRange(1, 31);
+    public static final int RANDOM_HOUR = Randoms.pickNumberInRange(0, 23);
+    public static final int RANDOM_MINUTE = Randoms.pickNumberInRange(0, 59);
+}
