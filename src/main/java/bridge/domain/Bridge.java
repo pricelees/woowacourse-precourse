e588ac726c3826850c7f7839758dcc6e072ad83e
sangdol <priceelees@gmail.com>
2023-12-12 22:48:08 +0900

    feat(Bridge): 유저가 건널 다리 정보를 저장하는 클래스 정의

---
+++ src/main/java/bridge/domain/Bridge.java
@@ -0,0 +1,33 @@
+package bridge.domain;
+
+import bridge.domain.Validator.BridgeSizeValidator;
+import bridge.domain.Validator.LocationValidator;
+import bridge.domain.Validator.MovingStepValidator;
+import bridge.util.BridgeMaker;
+import java.util.List;
+
+public class Bridge {
+    private final List<String> locationsCanMove;
+
+    public Bridge(List<String> locationsCanMove) {
+        locationsCanMove.forEach(LocationValidator::validate);
+        this.locationsCanMove = locationsCanMove;
+    }
+
+    public static Bridge from(BridgeMaker bridgeMaker, int size) {
+        BridgeSizeValidator.validate(size);
+        return new Bridge(bridgeMaker.makeBridge(size));
+    }
+
+    public boolean canMove(String location, int step) {
+        LocationValidator.validate(location);
+        MovingStepValidator.validate(step, locationsCanMove.size());
+
+        String answer = locationsCanMove.get(step);
+        return answer.equals(location);
+    }
+
+    public int getBridgeSize() {
+        return locationsCanMove.size();
+    }
+}
