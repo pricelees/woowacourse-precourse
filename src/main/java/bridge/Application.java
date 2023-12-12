7c2a2607a5ab7f80a7eafa232e07168c300c28fd
sangdol <priceelees@gmail.com>
2023-12-12 22:38:39 +0900

    feat(AppConfig): BridgeGame의 의존성을 관리하는 AppConfig 클래스 정의

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,12 @@
 package bridge;
 
-public class Application {
+import static bridge.config.AppConfig.bridgeGame;
 
+import camp.nextstep.edu.missionutils.Console;
+
+public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        bridgeGame().start();
+        Console.close();
     }
 }
