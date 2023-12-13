d9282b8f45e6fce31f51f8a5bccab54025742d0f
sangdol <priceelees@gmail.com>
2023-12-14 07:19:17 +0900

    refactor: use Singleton in ConsoleInputValidator class

--- src/main/java/org/example/view/input/ConsoleInputValidator.java
+++ src/main/java/org/example/view/input/ConsoleInputValidator.java
@@ -1,6 +1,14 @@
 package org.example.view.input;
 
 public final class ConsoleInputValidator {
-    public ConsoleInputValidator() {
+    private static ConsoleInputValidator instance;
+    private ConsoleInputValidator() {
+    }
+
+    public static ConsoleInputValidator getInstance() {
+        if (instance == null) {
+            instance = new ConsoleInputValidator();
+        }
+        return instance;
     }
 }
