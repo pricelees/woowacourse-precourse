4a1452e718d0b042e856a8206103b360bcc1927d
sangdol <priceelees@gmail.com>
2023-12-14 07:47:54 +0900

    feat: 페어의 정보를 저장하는 Pair 클래스

---
+++ src/main/java/pairmatching/domain/Pair.java
@@ -0,0 +1,43 @@
+package pairmatching.domain;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Pair {
+    private final Set<Crew> crews;
+
+    public Pair(Set<Crew> crews) {
+        this.crews = crews;
+    }
+
+    public List<String> getPairInfo() {
+        return crews.stream()
+                .map(Crew::getName)
+                .toList();
+    }
+
+    public List<String> getCrewsName() {
+        return crews.stream()
+                .map(Crew::getName)
+                .toList();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Pair pair = (Pair) o;
+        return crews.containsAll(pair.crews);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(crews);
+    }
+}
