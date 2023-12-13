f2fd8959546b4c3628b8f15d7a165e59e12a695c
sangdol <priceelees@gmail.com>
2023-12-14 07:19:01 +0900

    refactor: use Singleton in Reader class

--- src/main/java/org/example/view/input/Reader.java
+++ src/main/java/org/example/view/input/Reader.java
@@ -1,6 +1,18 @@
 package org.example.view.input;
 
 public final class Reader {
+    private static Reader instance;
+
+    private Reader() {
+    }
+
+    public static Reader getInstance() {
+        if (instance == null) {
+            instance = new Reader();
+        }
+        return instance;
+    }
+
     public String readLine() {
         return Console.readLine();
     }
