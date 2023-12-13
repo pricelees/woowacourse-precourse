e35ddfff30f56378526106e26579b613fc4a650f
sangdol <priceelees@gmail.com>
2023-12-14 07:46:26 +0900

    feat: 프로그램 실행 관련 상수가 정의된 ProgramOption 클래스

---
+++ src/main/java/pairmatching/constant/ProgramOption.java
@@ -0,0 +1,15 @@
+package pairmatching.constant;
+
+public class ProgramOption {
+    public static final String MATCH = "1";
+    public static final String SEARCH = "2";
+    public static final String CLEAR = "3";
+    public static final String QUIT = "Q";
+
+    public static final String REMATCH = "네";
+    public static final String NOT_REMATCH = "아니오";
+
+    public static final int INIT_MATCH_TRY_COUNT = 0;
+    public static final int MAX_MATCH_TRY_COUNT = 3;
+
+}
