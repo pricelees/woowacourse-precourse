4bfc39d3fbfd4a3f85d9dd08a02e91406dd20971
sangdol <priceelees@gmail.com>
2023-12-14 07:18:53 +0900

    refactor: use Singleton in Printer class

--- src/main/java/org/example/view/output/Printer.java
+++ src/main/java/org/example/view/output/Printer.java
@@ -2,6 +2,16 @@ package org.example.view.output;
 
 public final class Printer {
     private static final String LINE_SEPARATOR = System.lineSeparator();
+    private static Printer instance;
+    private Printer() {
+    }
+
+    public static Printer getInstance() {
+        if (instance == null) {
+            instance = new Printer();
+        }
+        return instance;
+    }
 
     public void print(Object input) {
         System.out.println(input);
