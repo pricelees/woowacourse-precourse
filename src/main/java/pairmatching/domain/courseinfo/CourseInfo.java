211c5d052ae58984380d711c49e863b99d2f43cd
sangdol <priceelees@gmail.com>
2023-12-14 07:49:29 +0900

    feat: 과정, 레벨, 미션을 하나로 묶어 저장하는 CourseInfo 클래스

---
+++ src/main/java/pairmatching/domain/courseinfo/CourseInfo.java
@@ -0,0 +1,37 @@
+package pairmatching.domain.courseinfo;
+
+import java.util.Objects;
+
+public class CourseInfo {
+    private final Course course;
+    private final Level level;
+    private final Mission mission;
+
+    public CourseInfo(Course course, Level level, Mission mission) {
+        this.course = course;
+        this.level = level;
+        this.mission = mission;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        CourseInfo that = (CourseInfo) o;
+        return course == that.course && level == that.level && mission == that.mission;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(course, level, mission);
+    }
+
+}
