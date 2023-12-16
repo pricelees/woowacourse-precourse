8475a52e97c09851bf3a70ea78ff1956a78a06a6
sangdol <priceelees@gmail.com>
2023-12-16 16:37:41 +0900

    feat: 입력을 담당하는 InputView 클래스와, 입력에 사용되는 Reader 클래스

---
+++ src/main/java/oncall/view/input/Reader.java
@@ -0,0 +1,21 @@
+package oncall.view.input;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public final class Reader {
+    private static Reader instance;
+
+    private Reader() {
+    }
+
+    public static Reader getInstance() {
+        if (instance == null) {
+            instance = new Reader();
+        }
+        return instance;
+    }
+
+    public String readLine() {
+        return Console.readLine();
+    }
+}
