086f6591df9b765a49f58d78d9e7d62e34d97e1a
sangdol <priceelees@gmail.com>
2023-12-16 16:32:09 +0900

    feat: 의존성 주입으로 Controller 객체를 생성하는 AppConfig 클래스

--- src/main/java/oncall/Application.java
+++ src/main/java/oncall/Application.java
@@ -1,7 +1,9 @@
 package oncall;
 
+import static oncall.config.AppConfig.controller;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        controller().run();
     }
 }
